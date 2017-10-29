e962c829f8ba0eaf16412fec2c4a73c577249890
Priyanshu Jain <priyanshu@launchyard.com>
2017-10-30 00:39:27 +0530

    Add production build and deploy

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import mainReducer from '../modules/main/reducers'
-
-
-const rootReducer = combineReducers({
-	mainReducer
-})
-
-export default rootReducer
