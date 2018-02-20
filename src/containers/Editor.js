f1681916288d6995dbbf92ea4bb6b74ba0a9d8fa
Priyanshu Jain <priyanshu@launchyard.com>
2018-02-20 22:56:23 +0530

    Alignment fixes

--- src/containers/Editor.js
+++ src/containers/Editor.js
@@ -1,77 +1,80 @@
-import React, { Component } from 'react'
-import ReactQuill from 'react-quill'
-import 'react-quill/dist/quill.snow.css'
+import React, { Component } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 export default class Editor extends Component {
   constructor(props) {
-    super(props)
-    const storedData = JSON.parse(localStorage.getItem('data'))
+    super(props);
+    const storedData = JSON.parse(localStorage.getItem("data"));
     this.state = {
-      value: `${storedData ? storedData : ''}`,
-      theme: 'snow',
-    }
-    this.handleChange = this.handleChange.bind(this)
+      value: `${storedData ? storedData : ""}`,
+      theme: "snow"
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(value) {
-    this.setState({ value })
-    localStorage.setItem('data', JSON.stringify(value))
+    this.setState({ value });
+    localStorage.setItem("data", JSON.stringify(value));
   }
 
   render() {
-    const { value } = this.state
+    const { value } = this.state;
     return (
-      <div>
-        <div className="input-field">
-                  <ReactQuill
-                    theme={this.state.theme}
-                    onChange={(value) => this.handleChange(value)}
-                    value={this.state.value}
-                    modules={Editor.modules}
-                    formats={Editor.formats}
-                    bounds={'.app'}
-                    placeholder={this.props.placeholder}
-                  />
-                </div>
+      <div className="input-field">
+        <ReactQuill
+          theme={this.state.theme}
+          onChange={value => this.handleChange(value)}
+          value={this.state.value}
+          modules={Editor.modules}
+          formats={Editor.formats}
+          bounds={".app"}
+          placeholder={this.props.placeholder}
+        />
       </div>
-    )
+    );
   }
 }
 
 Editor.modules = {
   toolbar: [
-    [{ header: '1' }, { header: '2' }, { font: [] }],
+    [{ header: "1" }, { header: "2" }, { font: [] }],
     [{ size: [] }],
-    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-    [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
-    ['link', 'image', 'video'],
-    ['clean'],
-  ],
-}
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" }
+    ],
+    ["link", "image", "video"],
+    ["clean"]
+  ]
+};
 /*
  * Quill editor formats
  * See https://quilljs.com/docs/formats/
  */
 Editor.formats = [
-  'header',
-  'font',
-  'size',
-  'bold',
-  'italic',
-  'underline',
-  'strike',
-  'blockquote',
-  'list',
-  'bullet',
-  'indent',
-  'link',
-  'image',
-  'video',
-]
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "video"
+];
 
 /*
  * PropType validation
  */
 Editor.propTypes = {
-  placeholder: React.PropTypes.string,
-}
+  placeholder: React.PropTypes.string
+};
